df296e2f8f7322dddad930bf4c8e987fc70e8fde
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-07-08 13:28:14 +0100

    fix:

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,23 @@ import "reflect-metadata";
 import "./shared/container";
 import { routes } from "routes";
 import cors from "cors";
+
 const app = express();
+
 app.use(express.json());
+
 app.use("/static", express.static(__dirname + "/tmp"));
-app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:3000"] }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], 
+    credentials: true,
+  }),
+);
+
 app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 app.use(routes);
+
 app.listen(3333, "0.0.0.0", () => {
   console.log("Server started on port 3333");
 });
